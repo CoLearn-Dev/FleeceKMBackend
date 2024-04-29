653814c7c383cabcadc917c9e2bf860ccdeaf019
nociza <azicon@berkeley.edu>
2024-04-29 00:32:06 -0700

    Update start.sh script to include PATH export and run uvicorn with poetry

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,1 +1,3 @@
-poetry run uvicorn fleecekmbackend.main:app --host 0.0.0.0 --port 12345
+export PATH="$HOME/.local/bin:$PATH"
+
+ poetry run uvicorn fleecekmbackend.main:app --host 0.0.0.0 --port 12345
